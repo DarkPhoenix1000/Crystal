03a4fbfab03a714ad1be8270b7e1032ee530ece4
DarkPhoenix1000 <82330796+DarkPhoenix1000@users.noreply.github.com>
2024-11-09 17:41:54 +0900

    Update mismagius.asm

--- data/pokemon/base_stats/mismagius.asm
+++ data/pokemon/base_stats/mismagius.asm
@@ -1,21 +1,21 @@
-+	db MISMAGIUS ; 252
-+
-+	db 60,  60,  60,   105,  105,  105
-+	;   hp  atk  def  spd  sat  sdf
-+
-+	db GHOST, GHOST ; type
-+	db 45 ; catch rate
-+	db 35 ; base exp
-+	db LEFTOVERS, LEFTOVERS ; items
-+	db GENDER_F50 ; gender ratio
-+	db 100 ; unknown 1
-+	db 25 ; step cycles to hatch
-+	db 5 ; unknown 2
-+	INCBIN "gfx/pokemon/mismagius/front.dimensions"
-+	dw NULL, NULL ; unused (beta front/back pics)
-+	db GROWTH_FAST ; growth rate
-+	dn EGG_NONE, EGG_NONE ; egg groups
-+
-+	; tm/hm learnset
-+	tmhm TOXIC, PSYCH_UP, HIDDEN_POWER, SUNNY_DAY, HYPER_BEAM, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, THUNDER, RETURN, PSYCHIC_M, SHADOW_BALL, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, REST, ATTRACT, THUNDERBOLT, DREAM_EATER, SUBSTITUTE, SHADOW_BALL
-+	; end
+	db MISMAGIUS ; 252
+
+	db 60,  60,  60,   105,  105,  105
+	;   hp  atk  def  spd  sat  sdf
+
+	db GHOST, GHOST ; type
+	db 45 ; catch rate
+	db 35 ; base exp
+	db LEFTOVERS, LEFTOVERS ; items
+	db GENDER_F50 ; gender ratio
+	db 100 ; unknown 1
+	db 25 ; step cycles to hatch
+	db 5 ; unknown 2
+	INCBIN "gfx/pokemon/mismagius/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_FAST ; growth rate
+	dn EGG_NONE, EGG_NONE ; egg groups
+
+  ; tm/hm learnset
+  tmhm TOXIC, PSYCH_UP, HIDDEN_POWER, SUNNY_DAY, HYPER_BEAM, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, THUNDER, RETURN, PSYCHIC_M, SHADOW_BALL, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, REST, ATTRACT, THUNDERBOLT, DREAM_EATER, SUBSTITUTE, SHADOW_BALL
+  ; end
