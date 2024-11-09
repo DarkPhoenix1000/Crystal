c8a000fe7ccdbf5cf7dabfe217ef8108157ce80b
DarkPhoenix1000 <82330796+DarkPhoenix1000@users.noreply.github.com>
2024-11-09 21:40:40 +0900

    Add files via upload

---
+++ gfx/pokemon/mismagius/frames.asm
@@ -0,0 +1,15 @@
+	dw .frame1
+	dw .frame2
+	dw .frame3
+	dw .frame4
+.frame1
+	db $00 ; bitmask
+	db $31, $32
+.frame2
+	db $00 ; bitmask
+	db $33, $34
+.frame3
+	db $00 ; bitmask
+	db $35, $36
+.frame4
+	db $01 ; bitmask
